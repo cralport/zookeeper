60ad5ba10c3d205c493f11c4b2f10e9eb1658ef3
cory alport <cralport@gmail.com>
2021-04-01 18:21:07 -0600

    finished front-end portion

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const { animals } = require('./data/animals');
+const { S_IFBLK } = require('node:constants');
+const { platform } = require('node:os');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -9,6 +11,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 //parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArrary = [];
@@ -108,6 +111,21 @@ app.post('/api/animals', (req, res) => {
     }
 });
 
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}`);
